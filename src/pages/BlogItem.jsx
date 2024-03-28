00543f4e154c0ee38af1ef9a51a6558771a76193
Eduardo Garza <eduardo@garza.ca>
2024-03-28 16:32:39 -0700

    Prod

--- src/pages/BlogItem.jsx
+++ src/pages/BlogItem.jsx
@@ -1,38 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
 import RootLayout from "../components/RootLayout";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
 import { ALL_BLOGS } from "../constants/ALL_BLOGS";
 
 export default function BlogItem() {
   const { slug } = useParams();
   const [blog, setBlog] = useState(null);
-  const [blogMetadata, setBlogMetadata] = useState(null);
-
-  console.log("New");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBlogContent = async () => {
-      const blogObject = ALL_BLOGS.find((blog) => blog.slug === slug);
-      const fileName = blogObject.fileName;
+      try {
+        const blogObject = ALL_BLOGS.find((blog) => blog.slug === slug);
+        const fileName = blogObject.fileName;
 
-      // Dynamically import the markdown file
-      const markdownFile = await import(`../constants/content/${fileName}`);
-      
-      // Read the content from the imported markdown file
-      const response = await fetch(markdownFile.default);
-      const markdownContent = await response.text();
-      
-      setBlog(markdownContent);
+        // Dynamically import the markdown file
+        const markdownFile = await import(`../constants/content/${fileName}`);
+
+        // Read the content from the imported markdown file
+        const response = await fetch(markdownFile.default);
+        const markdownContent = await response.text();
+
+        setBlog(markdownContent);
+      } catch (error) {
+        console.error("Error fetching blog content:", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchBlogContent();
-  }, []);
+  }, [slug]);
+
+  if (loading) {
+    return <RootLayout>Loading...</RootLayout>;
+  }
 
   if (!blog) {
-    return <RootLayout>Loading...</RootLayout>;
+    return <RootLayout>Blog not found</RootLayout>;
   }
 
   const MarkdownWrapper = styled.div`
