6751914d6128947cf86554b1057ab650a8fe4efa
Eduardo Garza <eduardo@garza.ca>
2024-03-27 20:35:37 -0700

    asd

---
+++ src/pages/hardware.js
@@ -0,0 +1,5 @@
+import RootLayout from "@/app/layout";
+
+export default function HardwarePage() {
+  return <RootLayout>Hardware</RootLayout>;
+}
---
+++ src/pages/software.js
@@ -0,0 +1,5 @@
+import RootLayout from "@/app/layout";
+
+export default function SoftwarePage() {
+  return <RootLayout>Software</RootLayout>;
+}
