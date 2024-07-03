eddb4083b7d6632ec69440f00016977a7a2299af
Eduardo Garza <eduardo@garza.ca>
2024-07-03 12:20:50 -0700

    Updated titles

--- src/constants/blogs/software/frontEndBlogs.js
+++ src/constants/blogs/software/frontEndBlogs.js
@@ -2,62 +2,62 @@ import { BLOG_CATEGORIES } from "../../blogCategories";
 
 export const REACT_BLOGS = [
   { 
-    title: "Reusable React Code Blocks for Super Fast App Development - Part 1", 
+    title: "React in Blocks - Part 1", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-blocks-for-super",
   },
   { 
-    title: "Reusable React Code Blocks, Part 2 - Styling and Components", 
+    title: "React in Blocks, Part 2 - Styling and Components", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part",
   },
   { 
-    title: "Reusable React Code Blocks, Part 3 - Routing and Navigation", 
+    title: "React in Blocks, Part 3 - Routing and Navigation", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-c13",
   },
   { 
-    title: "Reusable React Code Blocks, Part 4 - Authentication and Authorization", 
+    title: "React in Blocks, Part 4 - Authentication and Authorization", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-4d5",
   },
   { 
-    title: "Reusable React Code Blocks, Part 5 - Forms and Validation", 
+    title: "React in Blocks, Part 5 - Forms and Validation", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-cbf",
   },
   { 
-    title: "Reusable React Code Blocks, Part 6 - HTTP Client and Requests to APIs", 
+    title: "React in Blocks, Part 6 - HTTP Client and Requests to APIs", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-b44",
   },
   { 
-    title: "Reusable React Code Blocks, Part 7 - App State", 
+    title: "React in Blocks, Part 7 - App State", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-133",
   },
   { 
-    title: "Reusable React Code Blocks, Part 8 - Offline State", 
+    title: "React in Blocks, Part 8 - Offline State", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-276",
   },
   { 
-    title: "Reusable React Code Blocks, Part 9 - Logging", 
+    title: "React in Blocks, Part 9 - Logging", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-44e",
   },
   { 
-    title: "Reusable React Code Blocks, Part 10 - Analytics and SEO", 
+    title: "React in Blocks, Part 10 - Analytics and SEO", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-c8f",
   },
   { 
-    title: "Reusable React Code Blocks, Part 11 - Web Sockets", 
+    title: "React in Blocks, Part 11 - Web Sockets", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-eb0",
   },
   { 
-    title: "Reusable React Code Blocks, Part 12 - File Upload and Download", 
+    title: "React in Blocks, Part 12 - File Upload and Download", 
     category: BLOG_CATEGORIES.FRONT_END,
     url: "https://j2k8zv.substack.com/p/reusable-react-code-modules-part-8d7",
   },  
--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -18,18 +18,18 @@ export default function AboutPage() {
       <section className="mt-20 mb-6">
         <h1 className="text-3xl font-semibold mb-6">About Me</h1>
         {aboutMe.map((item, index) => (
-          <li className="text-gray-900 mt-2 leading-6 text-sm" key={index}>
+          <li className="text-gray-900 mt-2 leading-[20px] text-xs" key={index}>
             {item}
           </li>
         ))}
       </section>
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-4">Work Experience</h2>
-        <p className="text-gray-900 mt-2 leading-6 text-sm italic mb-2">
+        <p className="text-gray-900 mt-2 text-xs italic mb-2">
           See my full work experience on LinkedIn
         </p>
         <a
-          className="text-sm text-blue-600 underline hover:text-blue:800"
+          className="text-xs text-blue-600 underline hover:text-blue:800"
           href="https://www.linkedin.com/in/eduardocgarza"
         >
           LinkedIn - Eduardo Garza
