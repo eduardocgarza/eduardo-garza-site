8da40e36b1ae546a08c9c3c99313a2d28dd0536c
Eduardo Garza <eduardo@garza.ca>
2026-08-08 15:02:41 -0700

    Add external icon to UBC link

--- src/lib/components/AboutPage.svelte
+++ src/lib/components/AboutPage.svelte
@@ -21,12 +21,27 @@
         <span>{item.text}</span>
         {#if item.link}
           <a
-            class="font-medium text-gray-900 underline-offset-2 hover:underline"
+            class="inline-flex items-center font-medium text-gray-900 underline-offset-2 hover:underline"
             href={item.link.url}
             rel="noreferrer"
             target="_blank"
           >
             {item.link.text}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"
+            >
+              <path d="M7 7h10v10" />
+              <path d="M7 17 17 7" />
+            </svg>
           </a>
         {/if}
       </li>
